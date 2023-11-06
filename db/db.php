caa1bdaab0b2411df5df4273f208971e8d7dbf2b
R-G2110 <argeldelacruz86@gmail.com>
2023-11-06 14:45:12 +0100

    [Add]: cards on file index

--- db/db.php
+++ db/db.php
@@ -1,8 +1,12 @@
 <?php
 
+
 $movies = [
-	new Movie ("Five Nights at Freddy's", "Una guardia giurata problematica inizia a lavorare da Freddy Fazbear's Pizza. Mentre trascorre la sua prima notte di lavoro, si rende conto che il turno di notte da Freddy non sarà così facile da superare.", 'horror', 5, new Media('five_nights_at_freddys.jpg', "Five Nights at Freddy's")),
+	new Movie("Five Nights at Freddy's", "Una guardia giurata problematica inizia a lavorare da Freddy Fazbear's Pizza. Mentre trascorre la sua prima notte di lavoro, si rende conto che il turno di notte da Freddy non sarà così facile da superare.", 'horror', 5, new Media('five_nights_at_freddys.jpg', "Five Nights at Freddy's")),
+
 	new Movie ("Assassinio a Venezia", "Ora in pensione, ritiratosi in un esilio auto-imposto nella città più glamour del mondo, Poirot partecipa controvoglia a una seduta in un palazzo decadente e infestato. Quando uno degli ospiti viene ucciso, il detective si ritrova in un mondo di ombre e segreti.", 'suspense', 4, new Media('assasinio_a_venezia.jpg', "Assassinio a Venezia")),
+
 	new Movie ("Mission Impossible - Dead Reckoning - Parte uno", "Ethan Hunt e la sua squadra dell'IMF si trovano di fronte alla sfida più pericolosa che abbiano mai affrontato: trovare e disinnescare una nuova terrificante arma che minaccia l’ intera umanità. Con il destino del mondo e il controllo del futuro appesi a un filo, la squadra inizierà una frenetica missione in tutto il mondo, per impedire che l’arma cada nelle mani sbagliate. Messo di fronte a un nemico misterioso e onnipotente, tormentato da forze oscure del passato, Ethan sarà costretto a decidere se sacrificare tutto per questa missione, comprese le vite di coloro che gli stanno più a cuore.", 'azione', 4, new Media('mission_impossible_dead_reckoning_pt1.jpg', "Mission Impossible - Dead Reckoning - Parte uno")),
-	new Movie ("Saw X", "Nel film, ambientato nell'arco temporale che unisce SAW I e SAW II, John, malato e disperato, si reca in Messico per sottoporsi a una rischiosa procedura medica sperimentale nella speranza di una cura miracolosa per il suo cancro. Il suo sogno svanisce quando scopre che l’intera operazione non è altro che una truffa per frodare i più vulnerabili. Con un nuovo scopo, vendicarsi dei suoi truffatori, il famigerato serial killer torna al suo lavoro con il suo subdolo e inconfondibile stile.", 'suspense', 4, new Media('saw_x.jpg', "Saw X")),
-]
+
+	new Movie ("Saw X", "Nel film, ambientato nell'arco temporale che unisce SAW I e SAW II, John, malato e disperato, si reca in Messico per sottoporsi a una rischiosa procedura medica sperimentale nella speranza di una cura miracolosa per il suo cancro. Il suo sogno svanisce quando scopre che l’intera operazione non è altro che una truffa per frodare i più vulnerabili. Con un nuovo scopo, vendicarsi dei suoi truffatori, il famigerato serial killer torna al suo lavoro con il suo subdolo e inconfondibile stile.", 'suspense', 4, new Media('saw_x.jpg', "Saw X"))
+];
