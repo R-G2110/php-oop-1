7b1ca9704c71f7c091a96fa7c79a63417f459901
R-G2110 <argeldelacruz86@gmail.com>
2023-11-07 14:46:22 +0100

    [Add]: class TvSerie

---
+++ Model/TvSerie.php
@@ -0,0 +1,18 @@
+<?php
+
+class TvSerie extends Production {
+	public $aired_from_year;
+	public $aired_to_year;
+	public $number_of_episodes;
+	public $number_of_seasons;
+
+	public function __construct(string $title, string $desc, array $genre, float $rating, Media $image = null, string $aired_from_year, string $aired_to_year, string $number_of_episodes, string $number_of_seasons){
+
+    $this->aired_from_year = $aired_from_year;
+    $this->aired_to_year = $aired_to_year;
+    $this->number_of_episodes = $number_of_episodes;
+    $this->number_of_seasons = $number_of_seasons;
+
+    parent::__construct($title, $desc, $genre, $rating, $image);
+  }
+}
